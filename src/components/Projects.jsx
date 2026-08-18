02d35a4586a3509f0ae3c4032da8d4e8f3169bbd
G-morenoB <ubaldogaelcein@gmail.com>
2026-08-18 12:14:12 -0600

    nuevo proyecto

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -36,6 +36,14 @@ function Projects() {
       github: "https://github.com/G-morenoB/inventario-deportivo",
       demo: "https://zonabasquet-app.vercel.app/catalogo"
     },
+    {
+      image: "/img/magicideas.png",
+      title: "Magic Ideas - Desayunos Sorpresa (En desarrollo)",
+      description: "Tienda Online de Desaynos sorpesa",
+      tech: ["WordPress", "WooCommerce", "PHP"],
+      github: "",
+      demo: "https://silver-bee-938985.hostingersite.com/"
+    },
 
   ];
 
